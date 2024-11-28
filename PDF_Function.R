5125a92197de74d5e30e4a9befc30b9ebd171aa5
M-Zondo <mpumizondo97@gmail.com>
2024-11-28 12:05:23 +0200

    Updated

--- PDF_Function.R
+++ PDF_Function.R
@@ -7,12 +7,13 @@ pdf_func <- function(distribution = 'normal', params = c(param_1 , param_2)){
     return(pdf_pop)
     
   } else if (distribution == 'lognormal') {
+    
     pdf_pop <- function(x){
       dlnorm(x, meanlog = params[[1]], sdlog = params[[2]])
     }
     return(pdf_pop)
     
-  } else {
+  } else if (distribution == 'uniform'){
     pdf_pop <- function(x){
       dunif(x,min = params[[1]], max = params[[2]])
     }
--- RR_Function.R
+++ RR_Function.R
@@ -1,13 +1,13 @@
-rr_function <- function(distribution = 'linear', relrisk = 1.1, min_exposure = 10, scaling = 1) {
+rr_function <- function(distribution = 'linear', relrisk , min_exposure , scaling = 1) {
   
   if (distribution == 'linear') {
     relrisk_fun <- function(x){
-      ifelse(x < min_exposure, 1, relrisk*(x - min_exposure))
+      ifelse(x <= min_exposure, 1, relrisk*(x - min_exposure))
     } 
     return(relrisk_fun)
   } else if (distribution == 'exponential') {
     relrisk_fun <- function(x) {
-      ifelse(x < min_exposure,1,relrisk^((x - min_exposure)/scaling))
+      ifelse(x <= min_exposure,1,relrisk^((x - min_exposure)/scaling))
     }
     return(relrisk_fun)
   }
